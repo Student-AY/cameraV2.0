fedb513325ac5e2e1e40017796c9e51f8b66393d
Student-AY <Student-Ahmet-Yilmaz@hotmail.com>
2022-02-21 13:49:19 +0100

    useState en useEffect klaar

--- cameraAppNew/App.js
+++ cameraAppNew/App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
+import React , {useEffect , useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera  } from 'expo-camera'
+
+const [hasCameraPermission , setHasCameraPermission] = useState(null);  
+
+const [camera , setCamera] = useState(null);  
+
+const [image , setImage] = useState(null);  
+
+const [ type , setType] = useState(Camera.Constants.Type.back);  
+
+
+useEffect(() => {
+  (async () => {
+    const cameraStatus = await Camera.requestCameraPermissionsAsync();
+    setHasCameraPermission(cameraStatus);
+  })
+},[])
+
+if(hasCameraPermission === false){
+  return <Text>permission denied!</Text>
+}
+
+const takePicture = async () => {
+  if(camera){
+    const data = await camera.takePictureAsync(null);
+    setImage(data.uri)
+  }
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{}}>
+      
     </View>
   );
 }
